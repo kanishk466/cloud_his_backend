df9acfb63f9cd8bb2581709ded23cb304ac0590b
Sansar01 <st9889477@gmail.com>
2026-08-24 11:49:47 +0530

    dto updated and schema updated

---
+++ prisma/migrations/20260823065820_add_patient_added_optional_property/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "patients" ADD COLUMN     "companyName" TEXT,
+ADD COLUMN     "consultingDoctor" TEXT,
+ADD COLUMN     "coverage" TEXT,
+ADD COLUMN     "department" TEXT,
+ADD COLUMN     "empId" TEXT;
