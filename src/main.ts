8de06bdd074ecc7203ff1b9fb5d9fe10c1c6ed0a
Sansar Tiwari <89459552+Sansar01@users.noreply.github.com>
2026-09-22 20:56:20 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
       'http://localhost:5173',
        'http://localhost:3000',
       'http://localhost:8081',
-      'https://ojas1his.netlify.app',
+      'https://ojasadmin.netlify.app',
      'https://ojascarehis.netlify.app',
       'https://mediops-admin-ui.harshalvermaaaaa.workers.dev',
     ], // frontend URL (or use '*' for all origins)
